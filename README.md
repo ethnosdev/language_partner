52a1835befec78c0d7be0842306ced5fdbe64782
ethnosdev <ethnosdev@proton.me>
2023-06-14 17:45:30 +0800

    basic functionality working

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-# language_partner
+# Language Partner
 
-A new Flutter project.
+An app to help language learners get language practice.
 
-## Getting Started
+## Setup
 
-This project is a starting point for a Flutter application.
+Create a file with the API key:
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+```dart
+// lib/services/api_key.dart
+class ApiKey {
+  static const chatGpt = 'YOUR_KEY_HERE';
+}
+```
--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,3 +1,8 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:language_partner/services/api_key.dart';
+
 abstract interface class ChatService {
   Future<String> send(String message);
 }
@@ -5,6 +10,30 @@ abstract interface class ChatService {
 class ChatGpt implements ChatService {
   @override
   Future<String> send(String message) async {
-    return 'Howdy';
+    final url = Uri.parse('https://api.openai.com/v1/chat/completions');
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ${ApiKey.chatGpt}',
+    };
+    final data = {
+      'model': 'gpt-3.5-turbo',
+      'messages': [
+        {'role': 'system', 'content': 'You are a helpful assistant.'},
+        {'role': 'user', 'content': message}
+      ]
+    };
+
+    final response =
+        await http.post(url, headers: headers, body: jsonEncode(data));
+
+    if (response.statusCode == 200) {
+      final responseBody = jsonDecode(response.body);
+      // Process the response body as needed
+      final messageContent = responseBody['choices'][0]['message']['content'];
+      print(responseBody.toString());
+      return messageContent;
+    } else {
+      return 'Request failed with status: ${response.statusCode}';
+    }
   }
 }
